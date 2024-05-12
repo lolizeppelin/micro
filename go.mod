fc87280df060f4e7d261e6699aeb73d7d2a41cb0
gcy <lolizeppelin@gmail.com>
2024-05-13 00:02:34 +0800

    jsonschema库更换

--- go.mod
+++ go.mod
@@ -5,13 +5,13 @@ go 1.19
 require (
 	github.com/golang/protobuf v1.5.3
 	github.com/google/uuid v1.3.1
-	github.com/invopop/jsonschema v0.12.0
 	github.com/minio/highwayhash v1.0.2
 	github.com/mitchellh/hashstructure/v2 v2.0.2
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/pkg/errors v0.8.1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/sony/gobreaker v0.5.0
+	github.com/swaggest/jsonschema-go v0.3.70
 	github.com/xeipuuv/gojsonschema v1.2.1-0.20201027075954-b076d39a02e5
 	go.etcd.io/etcd/api/v3 v3.5.12
 	go.etcd.io/etcd/client/v3 v3.5.12
@@ -30,6 +30,7 @@ require (
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/swaggest/refl v1.3.0 // indirect
 	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
--- utils/jsonschema.go
+++ utils/jsonschema.go
@@ -1,47 +1,13 @@
 package utils
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/invopop/jsonschema"
+	"github.com/swaggest/jsonschema-go"
 	"reflect"
 )
 
-func _formatSchema(node *jsonschema.Schema, definitions map[string]*jsonschema.Schema) *jsonschema.Schema {
-	if node.Type == "array" {
-		if node.Items.Ref != "" {
-			node.Items = definitions[node.Items.Ref]
-			return node
-		}
-	} else if node.Type == "object" {
-		keys := map[string]*jsonschema.Schema{}
-		for pair := node.Properties.Newest(); pair != nil; pair = pair.Prev() {
-			n := _formatSchema(pair.Value, definitions)
-			if n != pair.Value {
-				keys[pair.Key] = n
-			}
-		}
-		for k, v := range keys {
-			node.Properties.Set(k, v)
-		}
-	} else if node.Ref != "" {
-		return definitions[node.Ref]
-	}
-	return node
-}
-
 func BuildJsonSchema(value reflect.Type) ([]byte, error) {
-	s := jsonschema.Reflect(reflect.New(value.Elem()).Interface())
-	definitions := map[string]*jsonschema.Schema{}
-	for k, v := range s.Definitions {
-		key := fmt.Sprintf("#/$defs/%s", k)
-		definitions[key] = v
-	}
-	for name, def := range definitions {
-		definitions[name] = _formatSchema(def, definitions)
-	}
-	root := definitions[s.Ref]
-	_formatSchema(root, definitions)
-	buff, err := json.Marshal(root)
-	return buff, err
+	r := jsonschema.Reflector{}
+
+	s, _ := r.Reflect(reflect.New(value.Elem()).Interface(), jsonschema.InlineRefs)
+	return s.MarshalJSON()
 }
