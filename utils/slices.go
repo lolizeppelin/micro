04af9ff9db708647f7ee2ec55915758e64b079eb
gcy <lolizeppelin@gmail.com>
2024-08-22 14:52:57 +0800

    slice反转方法

--- utils/slices.go
+++ utils/slices.go
@@ -8,3 +8,11 @@ func Supported(supports []string, target string) bool {
 	}
 	return false
 }
+
+func SliceReverse[T any](s []T) []T {
+	newS := make([]T, len(s))
+	for i, j := 0, len(s)-1; i <= j; i, j = i+1, j-1 {
+		newS[i], newS[j] = s[j], s[i]
+	}
+	return newS
+}
