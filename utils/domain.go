6f0b3070a86b84b6f4ca90914c75928b77b39fdc
gcy <lolizeppelin@gmail.com>
2024-08-27 10:51:18 +0800

    优化host-addr方法

--- utils/domain.go
+++ utils/domain.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"golang.org/x/net/publicsuffix"
 	"net"
 	"strings"
@@ -11,15 +12,28 @@ func TopLevelDomain(domain string) (string, error) {
 	return publicsuffix.EffectiveTLDPlusOne(domain)
 }
 
-func GetHostTLD(host string) (string, error) {
+func GetHostAddr(host string) (string, error) {
 	h, _, err := net.SplitHostPort(host)
 	if err != nil {
-		h = host // 如果没有端口号，直接使用 host
+		var e *net.AddrError
+		if errors.As(err, &e) {
+			h = e.Addr
+		} else {
+			return "", err
+		}
 	}
 	if strings.HasPrefix(h, "[") && strings.Contains(host, "]") {
 		// IPv6 地址
 		end := strings.Index(h, "]")
 		h = h[:end+1]
+	}
+	return h, nil
+}
+
+func GetHostTLD(host string) (string, error) {
+	h, err := GetHostAddr(host)
+	if err != nil {
+		return "", err
 	}
 	return TopLevelDomain(h)
 }
